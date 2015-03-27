bd5dde90400a4ecdbfea0f48f0fe83b3351fcea2
Christopher <ggmethos@autistici.org>
2015-03-26 21:17:15 -0400

    starting out with a bit of pseudo-code; save and load functions

---
+++ sp/src/sp/Sp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Acbn
+ */
+public class Sp extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       NewJFrame sp = new NewJFrame();
+       sp.setVisible(true);
+    }
+    
+}
